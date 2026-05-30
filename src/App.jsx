0000ebf9e31b901797bdc5072358fff3bf5b5ea1
ceesharptech <eniolaamusu6@gmail.com>
2026-05-30 16:10:41 +0100

    configuring vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Portfolio from "./Portfolio";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Portfolio />
+      <Analytics />
     </>
   );
 }
